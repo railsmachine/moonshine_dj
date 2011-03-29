40cc9dc2d8d107f1975d0492bbfdb498bef0b0b4
Will Farrington <will@railsmachine.com>
2011-03-29 17:29:55 -0400

    Make dj cap tasks always return true.

--- recipes/dj.rb
+++ recipes/dj.rb
@@ -2,20 +2,20 @@ namespace :god do
   namespace :dj do
     desc "start dj workers"
     task :start, :roles => :dj do
-      sudo 'god start dj'
+      sudo 'god start dj || true'
     end
     desc "stop dj workers"
     task :stop, :roles => :dj do
-      sudo 'god stop dj'
+      sudo 'god stop dj || true'
     end
     desc "restart dj workers"
     task :restart, :roles => :dj do
-      sudo 'god restart dj'
+      sudo 'god restart dj || true'
     end
 
     desc "show status of dj workers"
     task :status, :roles => :dj do
-      sudo 'god status dj'
+      sudo 'god status dj || true'
     end
   end
 end
