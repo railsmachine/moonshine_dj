294e4f3b72ca06fa6b70088e22d5292f001203e1
Josh Nichols <josh@railsmachine.com>
2013-01-22 20:44:10 +0000

    Fix error when configuration[:dj] is unset

--- lib/moonshine/dj.rb
+++ lib/moonshine/dj.rb
@@ -5,7 +5,7 @@ module Moonshine
         :workers => 1
       }
 
-      options = HashWithIndifferentAccess.new(default_options.merge(configuration[:dj]))
+      options = HashWithIndifferentAccess.new(default_options.merge(configuration[:dj] || {}))
 
       file "/etc/god/#{configuration[:application]}-dj.god",
         :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'dj.god.erb'), binding),
